5717c554ba268d38459bd5963726a6d55b3dde9c
alamin <asaduzzamanalamin2000@gmail.com>
2022-04-16 17:28:08 +0600

    boutta

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Home from './Pages/Home/Home/Home';
 import Header from './Pages/Shared/Header/Header';
 import Footer from './Pages/Shared/Footer/Footer';
 import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
+import NotFound from './Pages/Shared/NotFound/NotFound';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Login/Register/Register';
 
 function App() {
   return (
@@ -13,11 +16,14 @@ function App() {
       <Header></Header>
      <Routes>
        <Route path='/' element={<Home></Home>}></Route>
+       <Route path='/home' element={<Home></Home>}></Route>
        {/* dynamic route use params*/}
        <Route path ='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
        {/* dynamic route */}
-
+    <Route path='login' element={<Login></Login>}></Route>
+    <Route path='register' element={<Register></Register>}></Route>
        <Route path='/about' element={<About></About>}></Route>
+       <Route path='*' element={<NotFound></NotFound>}></Route>
      </Routes>
      <Footer></Footer>
     </div>
---
+++ src/Pages/Login/Register/Register.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css';
+
+const Register = () => {
+   const navigate = useNavigate();
+   const navigateLogIn = ()=>{
+      navigate('/login')
+   }
+   const handleRegister = event =>{
+      event.preventDefault();
+   }
+   return (
+      <div className='register-form'>
+         <h2 className='text-center'>plz register</h2>
+         <form onSubmit={handleRegister}>
+            <input type="text" name="name" id="" placeholder='your name'/>
+            
+           <input type="email" name="email" id="" placeholder='email adress' required />
+           
+           <input type="password" name="password" id="" placeholder='password' required/>
+           <input type="submit" value="Register" />
+
+         </form>
+         <p>Already have an account <Link to={'/login'} className='text-danger pe-auto text-decoration-none' onClick={navigateLogIn}>Please LogIn</Link></p>
+      </div>
+   );
+};
+
+export default Register;
